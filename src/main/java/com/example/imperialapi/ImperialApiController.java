da1d40643fcc44b8ae505d4928e7681e18c34749
YOUR NAME <YOUR_EMAIL>
2023-06-12 17:06:27 +0200

    add kilogram function and the belonging test case

--- src/main/java/com/example/imperialapi/ImperialApiController.java
+++ src/main/java/com/example/imperialapi/ImperialApiController.java
@@ -1,12 +1,17 @@
 package com.example.imperialapi;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImperialApiController {
     @RequestMapping("/api/gram")
-    public double convertToGram(double ounce){
+    public double convertToGram(@RequestParam double ounce){
         return ounce*28.35;
+    }
+    @RequestMapping("/api/kilogram")
+    public double convertToKilogram(double stone){
+        return stone*6.35;
     }
 }
